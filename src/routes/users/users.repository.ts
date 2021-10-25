d904b538eff7063b1ca09ec8e09bc29662d403df
Scott Abbey <scott@scottabbey.com>
2021-10-25 16:19:33 -0400

    wip

--- src/routes/users/users.repository.ts
+++ src/routes/users/users.repository.ts
@@ -38,31 +38,4 @@ export default class UsersRepository {
 		);
 		return rows;
 	}
-
-	public async findUserById(id: string) {
-		const result = await this.db.query<User>(
-			'SELECT * FROM users WHERE id = $1',
-			[id],
-		);
-		return result.rows[0];
-	}
-
-	public async findUserByEmail(email: string) {
-		const client = await this.db.connect();
-		const result = await client.query<User>(
-			'SELECT * FROM users WHERE email = $1',
-			[email],
-		);
-		await client.release();
-		return result.rows[0];
-	}
-
-	public async findAllUsers() {
-		const client = await this.db.connect();
-		const result = await client.query<User>(
-			'SELECT * FROM users WHERE active = true',
-		);
-		await client.release();
-		return result.rows;
-	}
 }
--- src/routes/users/users.service.ts
+++ src/routes/users/users.service.ts
@@ -1,3 +1,4 @@
+import { User } from './users.model';
 import UsersRepository from './users.repository';
 
 interface UsersServiceOptions {
@@ -12,6 +13,17 @@ export default class UsersService {
 	}
 
 	async findAllUsers() {
-		return await this.usersRepository.findAllUsers();
+		return await this.usersRepository.find();
+	}
+
+	async findUserById(id: string): Promise<User> {
+		return await this.usersRepository.find({ column: 'id', predicate: id })[0];
+	}
+
+	async findUserByEmail(email: string): Promise<User> {
+		return await this.usersRepository.find({
+			column: 'email',
+			predicate: email,
+		})[0];
 	}
 }
